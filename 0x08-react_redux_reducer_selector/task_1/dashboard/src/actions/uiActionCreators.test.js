26bee6d5b8271cd74e511ce8f3799b122d3c9818
imendy <enmaju37@gmail.com>
2024-02-29 08:46:00 +0100

    feat: react_redux_reducer_selector

---
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/actions/uiActionCreators.test.js
@@ -0,0 +1,38 @@
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
+import { login, logout, hideNotificationDrawer, displayNotificationDrawer } from "./uiActionCreators";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock";
+
+const middleWares = [thunk];
+const mockStore = configureStore(middleWares);
+
+describe("tests for UI notification action creators", () => {
+  it("should create proper action for login", () => {
+    const email = "james@gmail.com";
+    const password = "heheheh";
+
+    expect(login(email, password)).toEqual({
+      type: LOGIN,
+      user: { email: "james@gmail.com", password: "heheheh" },
+    });
+  });
+
+  it("should create proper action for logout", () => {
+    expect(logout()).toEqual({ type: LOGOUT });
+  });
+
+  it("should create proper action for displaying notification drawer", () => {
+    expect(displayNotificationDrawer()).toEqual({
+      type: DISPLAY_NOTIFICATION_DRAWER,
+    });
+  });
+
+  it("should create proper action for hiding notification drawer", () => {
+    expect(hideNotificationDrawer()).toEqual({
+      type: HIDE_NOTIFICATION_DRAWER,
+    });
+  });
+
+ 
+});
